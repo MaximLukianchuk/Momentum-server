925ffe45213c3403e04c5bf8320b355f5571fe99
mark-smirnov <mark-smirnov@yandex-team.ru>
2020-05-05 16:10:56 +0300

    token_security: добавлена подпись параметров запуска

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,8 +1,29 @@
+import qs from 'querystring';
+import crypto from 'crypto';
+
 export const auth = (req, res, next) => {
     try {
         const token = req.params.token;
+        const urlParams = req.query;
+        const ordered = {};
 
-        if (token !== process.env.API_TOKEN) {
+        Object.keys(urlParams).sort().forEach((key) => {
+            if (key.slice(0, 3) === 'vk_') {
+                ordered[key] = urlParams[key];
+            }
+        });
+
+        const stringParams = qs.stringify(ordered);
+        const paramsHash = crypto
+            .createHmac('sha256', process.env.API_SECRET_KEY)
+            .update(stringParams)
+            .digest()
+            .toString('base64')
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=$/, '');
+
+        if (token !== process.env.API_TOKEN || paramsHash !== urlParams.sign) {
             return res.status(401).json({ message: 'No authorisation' });
         }
         next();
